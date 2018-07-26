685b5a05d494663baab8f72ed963d39feb03cca1
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-07-26 16:05:57 +0200

    exo2p2 php

--- index.php
+++ index.php
@@ -7,10 +7,12 @@
   <body>
     <p>
 <?php
+/*Déclaration d'une variable en bollean*/
   $isEasy = false;
+  /*Condition si la variable vaut vrai*/
   if( $isEasy === true){
     echo 'C\'est facile !!';
-  } else {
+  } else {/*Autre condition si elle vaut faux*/
     echo 'C\'est difficile !!!';
   }
  ?>
