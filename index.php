3492cf12af6536fc9ac7ce42427b90e37c8c07c9
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-07-26 09:03:31 +0200

    exo2 partie 2 php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 2</title>
+  </head>
+  <body>
+    <p>
+<?php
+  $isEasy = false;
+  if( $isEasy === true){
+    echo 'C\'est facile !!';
+  } else {
+    echo 'C\'est difficile !!!';
+  }
+ ?>
+    </p>
+  </body>
+</html>
